880c775d15993b8e58307b029cc7f6a8f2641fa4
Juliya Sartasova <anjee2007@mail.ru>
2022-08-09 18:49:50 +0300

    день 1

---
+++ technoshop-cms/script/elems.js
@@ -0,0 +1,5 @@
+export const modalBtn = document.querySelector('.modal-open-btn');
+export const modal = document.querySelector('.modal');
+export const form = document.querySelector('.modal-content');
+export const preview = document.querySelector('.preview');
+export const formData = document.querySelectorAll('.form-control');
---
+++ technoshop-cms/script/modalController.js
@@ -0,0 +1,24 @@
+import { hidePreview } from "./previewController.js";
+import { form } from "./elems.js";
+
+const openModal = (modal, classOpen) => {
+    modal.classList.add(classOpen);
+};
+
+const closeModal = (modal, classOpen) => {
+    modal.classList.remove(classOpen);
+};
+
+export const modalController = ({ modal, btn, classOpen, classClose}) => {
+    btn.addEventListener('click', () => {
+        openModal(modal, classOpen);
+    });
+
+    modal.addEventListener('click', (event) => {
+        if(event.target === modal || event.target.classList.contains(classClose)) {
+            closeModal(modal, classOpen);
+            hidePreview();
+            form.reset();
+        }
+    });
+};
